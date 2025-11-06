3e6d63371503ac361651df75dc34b177ee575f95
ZoeEscobar <zoeescobar2207@gmail.com>
2025-11-06 18:31:35 -0300

    Cambié config para una mejora en el analisis de sentimiento (diferencia de palabras de emergencia), que tome bien la ruta del .json y para que incluya el umbral de confianza de sentimientos negativos

--- config.py
+++ config.py
@@ -3,12 +3,45 @@ import os
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', '[TELEGRAM_TOKEN]')
 GROQ_API_KEY = os.getenv('GROQ_API_KEY', '[API_KEY]')
 GROQ_API_URL = '[GROQ_URL]'
-DATASET_PATH = 'primeros_auxilios.json'
-
 DATASET_PATH = 'data/primeros_auxilios.json' 
 
-PALABRAS_EMERGENCIA = [
-    "sangrado", "corte", "herida", "quemadura", "asfixia", "atragantamiento",
-    "infarto", "desmayo", "convulsión", "epilepsia", "hipoglucemia", "shock",
-    "ahogo", "picadura", "mordedura", "fractura", "golpe", "lesión"
-]
+# palabras de alta severidad (esto lo pueden cambiar si quieren)
+
+PALABRAS_URGENCIA_ALTA = {
+    # emergencias vitales
+    "acv", "ataque", "infarto", "parocardiaco", "no puedo respirar", "respirar", 
+    "asfixia", "ahogo", "ahogando", "atragantamiento", "convulsión", "convulsionando", 
+    "shock", "emergencia", "urgente", "anafilaxia", "anafiláctico",
+
+    # sangrado
+    "sangre", "sangrado", "desangrando", "sangrar", "hemorragia", "amputado",
+    
+    # Trauma / Golpes (Mantenemos 'golpe' y 'fractura' como alta)
+    "golpe", "golpee", "golpeado", "fractura", "quebre", "accidente",
+    "lesión", "columna", "cuello", "médula",
+    
+    # pérdida de conciencia
+    "desmayo", "desmayar", "perdio el conocimiento", "inconsciente", "síncope",
+    
+    # dolor SEVERO 
+    "terrible", "muchísimo", "pecho", "insoportable", "agonía",
+    
+    # otras cosas graves
+    "parto", "grave", "severo", "intenso", "veneno", "envenenamiento", "electrocución",
+    "electrocutado", "rayo", "mordedura", "serpiente"
+}
+
+PALABRAS_URGENCIA_MEDIA = {
+    # dolor general o leve 
+    "dolor", "duele", "cabeza", "dolor de cabeza",
+    
+    # malestar general
+    "molestia", "incómodo", "preocupa", "malestar", "leve", "regular", "un poco",
+    "mareo", "náuseas", "fiebre", "tengo tos", "tos", "estornudo", "cansado", 
+    "gripe", "resfriado", "ansiedad", "vomito", "alergia", "picadura", "insecto",
+    "quemadura", "esguince", "distensión", "moretón", "rasguño", "abrasión",
+    "congestión", "garganta", "abdominal", "diarrea"
+}
+
+# umbral de confianza para considerar "alta confianza" en sentimientos negativos
+UMBRAL_CONFIANZA_NEG = 0.80
